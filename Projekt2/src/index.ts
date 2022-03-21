7e3c3aeffc2bbf4467b7e32e8b2e9ad630a11b0b
MattCobosco <mateuszkalucki@gmail.com>
2022-03-21 21:49:47 +0100

    done?

--- Projekt2/src/index.ts
+++ Projekt2/src/index.ts
@@ -1,72 +1,85 @@
-import express from 'express'
-import {Request, Response} from 'express'
-import Note from 'note'
+import express from 'express';
+import {Request, Response} from 'express';
+import Note from '../note';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
-})
-app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
-  res.sendStatus(200).send('POST Hello World')
-})
-
-let notes : Note[] = [];
-let currentId = 1;
+const notes : Note[] = [];
 
 // Dodanie nowej notatki
-app.post('/note', function(req, res){
-    let newNoteTitle = req.body.title;
-    let newNoteContent = req.body.content;
-    let newNoteCreateDate = req.body.createDate;
-    let newNoteTags = req.body.tags;
-    let newNoteId = currentId;
-
-    if(newNoteTitle === null || newNoteContent === null)
+app.post('/note', function(req : Request, res : Response)
+{
+    const note : Note = req.body;
+    if(note.title === undefined || note.content === undefined)
     {
         res.status(400).send('Missing note title/content')
     }
     else
     {
-        let newNote : Note = {
-            title : newNoteTitle,
-            content : newNoteContent,
-            createDate : newNoteCreateDate,
-            tags : newNoteTags,
-            id : newNoteId
-        }
-
-        notes.push(newNote)
-        res.status(201).send('New note added with ID:' + newNoteId)
-        currentId++
+        note.id=Date.now();
+        notes.push(note);
+        res.status(201).send(note.id);
     }
 })
-// TODO: getting id from url
+
 // Odczytanie notatki o danym id
-app.get('/note/:id', function(req,res){
-    const id = +req.query.id;
-    const jsonNote = notes.find(i=>i.id ===id)
-    const note = JSON.parse(jsonNote) 
+app.get('/note/:id', function(req : Request,res : Response)
+{
+  const note = notes.find(i=>i.id === req.body.id);
+  if(note===undefined)
+  {
+    res.status(404).send('Note of this id does not exist');
+  }
+  else
+  {
     res.status(200).send(note)
+  }
 })
 
-// TODO:
-app.put()
+// Edycja notatki o danym id
+app.put('/note/:id', function(req: Request, res: Response)
+{
+  const newNote: Note = req.body;
+  if(newNote.title === undefined || newNote.content === undefined || newNote.id === undefined)
+  {
+    res.status(400).send('Missing note title/content/id');
+  } 
+  else
+  {
+    let currentNote: Note = notes.find(i=>i.id === newNote.id)
+    if(currentNote === undefined)
+    {
+      res.status(404).send('Note of this id does not exist');
+    }
+    else
+    {
+      currentNote = newNote;
+      res.status(200).send(currentNote);
+    }
+  }
+})
 
 //TODO: getting id from url
 // Usunięcie notatki o danym id
-app.delete('/note/:id', function(req, res){
-  try {  
-    const id = +req.query.id
-    const removeIndex = notes.findIndex(i=>i.id ===id)
-    notes.splice(removeIndex,1)
-    res.status(200).send(`Notatka od id ${id} została usunięta pomyślnie`)
-    } catch (error) {
-      res.status(400).send(error)
-    }
+app.delete('/note/:id', function(req : Request, res : Response)
+{
+  const note: Note = notes.find(i=>i.id == req.body.id);
+  if(note === undefined)
+  {
+    res.status(400).send('Note of this id does not exist');
+  }
+  else
+  {
+    notes.splice(req.body.id, 1);
+    res.status(204).send('The requested note has been deleted')
+  }
 })
+
+function noteMissing(res : Response)
+{
+  res.status(404).send('Note of this id does not exist');
+}
 
 app.listen(3000)
