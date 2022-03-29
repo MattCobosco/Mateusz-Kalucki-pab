2367489c385d2d03023048e8a89d3d3dde7dd909
MattCobosco <mateuszkalucki@gmail.com>
2022-03-30 00:53:23 +0200

    lab3 beep bop bop

--- Projekt3/src/index.js
+++ Projekt3/src/index.js
@@ -12,12 +12,16 @@ app.post('/note', function (req, res) {
     if (note.title === undefined || note.content === undefined) {
         res.status(400).send('Missing note title/content');
     }
-    // TODO: Checking if any of the tags exists
     else {
-        note.id = Date.now();
-        notes.push(note);
-        res.status(201).send(note.id);
+        note.tags.forEach(function (tag) {
+            if (!tags.find(function (i) { return i.name === tag.name; })) {
+                tags.push(tag);
+            }
+        });
     }
+    note.id = Date.now();
+    notes.push(note);
+    res.status(201).send(note.id);
 });
 // Wyświetlenie listy notatek
 app.get('/notes', function (req, res) {
@@ -44,29 +48,25 @@ app.put('/note/:id', function (req, res) {
     if (newNote.title === undefined || newNote.content === undefined || newNote.id === undefined) {
         res.status(400).send('Missing note title/content/id');
     }
-    // TODO: Checking if any of the tags exists
     else {
         var currentNote = notes.find(function (i) { return i.id === newNote.id; });
-        for (var i = 0; i < currentNote.tags.length - 1; i++) {
-            for (var j = 0; j < tags.length - 1; i++) {
-                if (currentNote.tags[i] === tags[j]) {
-                    break;
-                }
-                tags.push(currentNote.tags[i]);
-            }
-        }
         if (currentNote === undefined) {
             res.status(404).send('Note of this id does not exist');
         }
         else {
             currentNote = newNote;
+            currentNote.tags.forEach(function (tag) {
+                if (!tags.find(function (i) { return i.name === tag.name; })) {
+                    tags.push(tag);
+                }
+            });
             res.status(200).send(currentNote);
         }
     }
 });
 // Usunięcie notatki o danym id
 app["delete"]('/note/:id', function (req, res) {
-    var note = notes.find(function (i) { return i.id == req.body.id; });
+    var note = notes.find(function (i) { return i.id === +req.params.id; });
     if (note === undefined) {
         res.status(400).send('Note of this id does not exist');
     }
@@ -82,11 +82,14 @@ app.post('/tag', function (req, res) {
     if (tag.name === undefined) {
         res.status(400).send('Missing tag name');
     }
-    else if (!contains(tags, tag.name.toLowerCase())) {
-        tag.id = tags.length - 1;
+    else if (tags.find(function (t) { return t.name === tag.name; })) {
+        res.status(400).send('Tag already exists');
+    }
+    else {
+        tag.id = Date.now();
         tag.name = tag.name.toLowerCase();
         tags.push(tag);
-        res.status(200).send(tag.id);
+        res.status(200).send(tag);
     }
 });
 // Wyświetlenie listy tagów
@@ -98,24 +101,42 @@ app.get('/tags', function (req, res) {
         res.status(200).send(tags);
     }
 });
+// Wyświetlenie tagu o danym id
+app.get('/tag/:id', function (req, res) {
+    var tag = tags.find(function (i) { return i.id === req.body.id; });
+    if (tag === undefined) {
+        res.status(404).send('Tag of this id does not exist');
+    }
+    else {
+        res.status(200).send(tag);
+    }
+});
+// Edycja tagu o danym id
+app.put('/tag/:id', function (req, res) {
+    var newTag = req.body;
+    if (newTag.name === undefined || newTag.id === undefined) {
+        res.status(400).send('Missing tag name/id');
+    }
+    else {
+        var currentTag = tags.find(function (i) { return i.id === newTag.id; });
+        if (currentTag === undefined) {
+            res.status(404).send('Tag of this id does not exist');
+        }
+        else {
+            currentTag = newTag;
+            res.status(200).send(currentTag);
+        }
+    }
+});
 //Usuniecie tagu z listy
 app["delete"]('tag/:id', function (req, res) {
-    var tag = tags.find(function (i) { return i.id == req.body.id; });
+    var tag = tags.find(function (i) { return i.id == +req.params.id; });
     if (tag === undefined) {
         res.status(400).send('The tag of this id does not exist');
     }
     else {
+        tags.splice(req.params.id, 1);
         res.status(204).send('The requested tag has been deleted');
     }
 });
-function contains(arr, obj) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].name === obj) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-}
 app.listen(3000);
