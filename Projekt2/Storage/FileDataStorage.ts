c771a436334a238948af64c7f695458b1db94cbb
MattCobosco <mateuszkalucki@gmail.com>
2022-04-20 11:01:12 +0200

    Note CRUD for file storage done

--- Projekt2/Storage/FileDataStorage.ts
+++ Projekt2/Storage/FileDataStorage.ts
@@ -2,13 +2,23 @@ import IDataStorage from './IDataStorage';
 import Note from '../Models/Note';
 import Tag from '../Models/Tag';
 import Storage from './Storage';
-import User from '../Models/User'
+import User from '../Models/User';
+import Repository from '../Repository';
 
+const repo : Repository = new Repository();
 let storage: Storage;
+
+repo.readStorage().then(data => 
+    {
+      if(data)
+        storage = JSON.parse(data);
+      else 
+        storage = new Storage();
+    });
 
 class FileDataStorage implements IDataStorage
 {
-    addNote(note: Note): void 
+    addNote(note: Note, user: User): void 
     {
         console.log(note);
         if(note.tags != undefined)
@@ -26,26 +36,48 @@ class FileDataStorage implements IDataStorage
             }
           });
       }
-      note.id=Date.now();
+      note.id = Date.now();
       storage.notes.push(note);
+      user.notesCreatedIds.push(note.id ?? 0);
+      repo.updateStorage(JSON.stringify(storage));
     }
 
     getNotes(registeredUser: User): Note[] 
     {
         return storage.notes.filter(n => registeredUser.notesCreatedIds.includes(n.id ?? 0));
     }
-    getNoteById(noteId: number): Note {
-        throw new Error('Method not implemented.');
+
+    getNoteById(noteId: number): Note 
+    {
+        return storage.notes.find(n => n.id === noteId);
     }
-    editNoteById(noteId: number): void {
-        throw new Error('Method not implemented.');
+
+    editNoteById(noteId: number, noteContent: Note): void 
+    {
+        const noteToEdit = storage.notes.find(n => n.id === noteId);
+        if(noteToEdit)
+        {
+            noteToEdit.title = noteContent.title;
+            noteToEdit.content = noteContent.content;
+            noteToEdit.tags = noteContent.tags;
+            repo.updateStorage(JSON.stringify(storage));
+        }
     }
+
     deleteNoteById(noteId: number): void {
-        throw new Error('Method not implemented.');
+        const noteToDelete = storage.notes.find(n => n.id === noteId);
+        if(noteToDelete)
+        {
+            storage.notes.splice(storage.notes.indexOf(noteToDelete), 1);
+            repo.updateStorage(JSON.stringify(storage));
+        }
     }
-    getPublicNotesByUsername(username: string): Note[] {
-        throw new Error('Method not implemented.');
+
+    getPublicNotesByUsername(username: string): Note[] 
+    {
+        return storage.notes.filter(n => n.private === false && this.getUserByUsername(username).notesCreatedIds.includes(n.id ?? 0));
     }
+
     addTag(tag: Tag): void {
         throw new Error('Method not implemented.');
     }
@@ -60,6 +92,10 @@ class FileDataStorage implements IDataStorage
     }
     deleteTagById(tagId: number): void {
         throw new Error('Method not implemented.');
+    }
+
+    getUserByUsername(username: string): User {
+        return storage.users.find(u => u.login === username)
     }
 }
 
--- Projekt2/Storage/dist/FileDataStorage.js
+++ Projekt2/Storage/dist/FileDataStorage.js
@@ -1,10 +1,20 @@
 "use strict";
 exports.__esModule = true;
+var Storage_1 = require("./Storage");
+var Repository_1 = require("../Repository");
+var repo = new Repository_1["default"]();
 var storage;
+repo.readStorage().then(function (data) {
+    if (data)
+        storage = JSON.parse(data);
+    else
+        storage = new Storage_1["default"]();
+});
 var FileDataStorage = /** @class */ (function () {
     function FileDataStorage() {
     }
-    FileDataStorage.prototype.addNote = function (note) {
+    FileDataStorage.prototype.addNote = function (note, user) {
+        var _a;
         console.log(note);
         if (note.tags != undefined) {
             note.tags.forEach(function (tag) {
@@ -19,21 +29,34 @@ var FileDataStorage = /** @class */ (function () {
         }
         note.id = Date.now();
         storage.notes.push(note);
+        user.notesCreatedIds.push((_a = note.id) !== null && _a !== void 0 ? _a : 0);
+        repo.updateStorage(JSON.stringify(storage));
     };
     FileDataStorage.prototype.getNotes = function (registeredUser) {
         return storage.notes.filter(function (n) { var _a; return registeredUser.notesCreatedIds.includes((_a = n.id) !== null && _a !== void 0 ? _a : 0); });
     };
     FileDataStorage.prototype.getNoteById = function (noteId) {
-        throw new Error('Method not implemented.');
+        return storage.notes.find(function (n) { return n.id === noteId; });
     };
-    FileDataStorage.prototype.editNoteById = function (noteId) {
-        throw new Error('Method not implemented.');
+    FileDataStorage.prototype.editNoteById = function (noteId, noteContent) {
+        var noteToEdit = storage.notes.find(function (n) { return n.id === noteId; });
+        if (noteToEdit) {
+            noteToEdit.title = noteContent.title;
+            noteToEdit.content = noteContent.content;
+            noteToEdit.tags = noteContent.tags;
+            repo.updateStorage(JSON.stringify(storage));
+        }
     };
     FileDataStorage.prototype.deleteNoteById = function (noteId) {
-        throw new Error('Method not implemented.');
+        var noteToDelete = storage.notes.find(function (n) { return n.id === noteId; });
+        if (noteToDelete) {
+            storage.notes.splice(storage.notes.indexOf(noteToDelete), 1);
+            repo.updateStorage(JSON.stringify(storage));
+        }
     };
     FileDataStorage.prototype.getPublicNotesByUsername = function (username) {
-        throw new Error('Method not implemented.');
+        var _this = this;
+        return storage.notes.filter(function (n) { var _a; return n.private === false && _this.getUserByUsername(username).notesCreatedIds.includes((_a = n.id) !== null && _a !== void 0 ? _a : 0); });
     };
     FileDataStorage.prototype.addTag = function (tag) {
         throw new Error('Method not implemented.');
@@ -49,6 +72,9 @@ var FileDataStorage = /** @class */ (function () {
     };
     FileDataStorage.prototype.deleteTagById = function (tagId) {
         throw new Error('Method not implemented.');
+    };
+    FileDataStorage.prototype.getUserByUsername = function (username) {
+        return storage.users.find(function (u) { return u.login === username; });
     };
     return FileDataStorage;
 }());
