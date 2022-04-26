130fc71ca630451bd262e07f8a9a6d6796da1e47
MattCobosco <mateuszkalucki@gmail.com>
2022-04-26 19:58:00 +0200

    crud for database

--- Projekt2/Storage/DatabaseDataStorage.ts
+++ Projekt2/Storage/DatabaseDataStorage.ts
@@ -2,64 +2,221 @@ import IDataStorage from './IDataStorage';
 import Note from '../Models/Note';
 import Tag from '../Models/Tag';
 import User from '../Models/User';
+import mongoose from 'mongoose';
+import * as fs from 'fs';
+var jsonConfig =JSON.parse(fs.readFileSync('../config.json', 'utf8'));
 
 export class DatabaseDataStorage implements IDataStorage
 {
-    addNote(note: Note, user: User): void {
-        throw new Error('Method not implemented.');
-    }
-    getNotes(): Note[] {
-        throw new Error('Method not implemented.');
-    }
-    getNoteById(noteId: number): Note {
-        throw new Error('Method not implemented.');
-    }
-    editNoteById(noteId: number, noteContent: Note): void {
-        throw new Error('Method not implemented.');
-    }
-    deleteNoteById(noteId: number): void {
-        throw new Error('Method not implemented.');
-    }
-    getPublicNotesByLogin(login: string): Note[] {
-        throw new Error('Method not implemented.');
-    }
-    shareNote(noteId: number, login: string): void {
-        throw new Error('Method not implemented.');
-    }
-    getNotesSharedToUserByLogin(login: string) : Note[] {
-        throw new Error('Method not implemented.');
+    // models for mongoose
+    noteModel = new mongoose.model('Notes', new mongoose.Schema(Note));
+    tagModel = new mongoose.model('Tags', new mongoose.Schema(Tag));
+    userModel = new mongoose.model('Users', new mongoose.Schema(User));
+
+    async populateDatabase(): Promise<void> 
+    {
+        // Check database connection
+        if(!mongoose.connection.readyState)
+        {
+            await mongoose.connect(jsonConfig.mongoConnectionString);
+        }
+
+        // Check if database is empty
+        // get all notes
+        const notes = await mongoose.connection.db.collection('notes').find({}).toArray();
+        // get all tags
+        const tags = await mongoose.connection.db.collection('tags').find({}).toArray();
+        // get all users
+        const users = await mongoose.connection.db.collection('users').find({}).toArray();
+
+        if(notes.length === 0 && tags.length === 0 && users.length === 0)
+        {
+            // Add tables for notes, tags and users
+            const noteModel = new mongoose.model('Notes', new mongoose.Schema(Note));
+            const tagModel = new mongoose.model('Tags', new mongoose.Schema(Tag));
+            const userModel = new mongoose.model('Users', new mongoose.Schema(User));
+
+            // Add some tags
+            const tag1 = new tagModel({
+                id: 1,
+                name: 'tag1'
+            });
+            await tag1.save();
+
+            const tag2 = new tagModel({
+                id: 2,
+                name: 'tag2'
+            });
+            await tag2.save();
+
+            const tag3 = new tagModel({
+                id: 3,
+                name: 'tag3'
+            });
+            await tag3.save();
+
+            // Add some notes
+            const note1 = new noteModel({
+                id: 1,
+                title: 'Note 1',
+                content: 'This is note 1',
+                createDate: '2020-01-01',
+                tags: ['tag1', 'tag2'],
+                private: false
+            });
+            await note1.save();
+
+            const note2 = new noteModel({
+                id: 2,
+                title: 'Note 2',
+                content: 'This is note 2',
+                createDate: '2020-01-02',
+                tags: ['tag1', 'tag3'],
+                private: true
+            });
+            await note2.save();
+
+            const note3 = new noteModel({
+                id: 3,
+                title: 'Note 3',
+                content: 'This is note 3',
+                createDate: '2020-01-03',
+                tags: ['tag2', 'tag3'],
+                private: false
+            });
+            await note3.save();
+
+            // Add some users
+            const user1 = new userModel({
+                id: 1,
+                login: 'user1',
+                password: 'user1',
+                notesCreatedIds: [1],
+                tagsCreatedIds: [1, 2],
+                isAdmin: true,
+                notesSharedToUserIds: [3]
+            });
+            await user1.save();
+
+            const user2 = new userModel({
+                id: 2,
+                login: 'user2',
+                password: 'user2',
+                notesCreatedIds: [2, 3],
+                tagsCreatedIds: [3],
+                isAdmin: false,
+                notesSharedToUserIds: [1]
+            });
+            await user2.save();
+        }
     }
 
-    addTag(tag: Tag, user : User): void {
-        throw new Error('Method not implemented.');
-    }
-    getTags(): Tag[] {
-        throw new Error('Method not implemented.');
-    }
-    getTagById(tagId: number): Tag {
-        throw new Error('Method not implemented.');
-    }
-    editTagById(tagId: number, tagContent: Tag): void {
-        throw new Error('Method not implemented.');
-    }
-    deleteTagById(tagId: number): void {
-        throw new Error('Method not implemented.');
+    addNote(note: Note, user: User): void 
+    {
+        this.noteModel.create(note);
+        if (note.id!==undefined)
+        user.notesCreatedIds.push(note.id);
     }
 
-    addUser(user: User): void {
-        throw new Error('Method not implemented.');
+    getNotes(): Note[] 
+    {
+        return this.noteModel.find({}).exec();
     }
-    editUserByLogin(login: string, user: User): void {
-        throw new Error('Method not implemented.');
+
+    getNoteById(noteId: number): Note 
+    {
+        return this.noteModel.findOne({id: noteId}).exec();
     }
-    getUserByLogin(login: string): User {
-        throw new Error('Method not implemented.');
+
+    editNoteById(noteId: number, noteContent: Note): void 
+    {
+        this.noteModel.findOneAndUpdate({id: noteId}, noteContent).exec();
     }
-    getUsers(): User[] {
-        throw new Error('Method not implemented.');
+
+    deleteNoteById(noteId: number): void 
+    {
+        this.noteModel.findOneAndDelete({id: noteId}).exec();
     }
-    deleteUserByLogin(login: string): void {
-        throw new Error('Method not implemented.');
+
+    getPublicNotesByLogin(login: string): Note[] 
+    {
+        let notes: Note[] = [];
+        const user = this.userModel.findOne({login: login}).exec();
+        if (user)
+        {
+            user.notesCreatedIds.forEach((noteId: number) => {
+                const note = this.noteModel.findOne({id: noteId}).exec();
+                if (note && !note.private)
+                {
+                    notes.push(note);
+                }
+            });
+        }
+        return notes;
+    }
+
+    shareNote(noteId: number, login: string): void
+    {
+        const user = this.userModel.findOne({login: login}).exec();
+        user.notesSharedToUserIds.push(noteId);
+        user.save();
+    }
+
+    getNotesSharedToUserByLogin(login: string) : Note[] 
+    {
+        return this.userModel.findOne({login: login}).exec().notesSharedToUserIds;
+    }
+
+
+    addTag(tag: Tag, user : User): void 
+    {
+        this.tagModel.create(tag);
+    }
+
+    getTags(): Tag[] 
+    {
+        return this.tagModel.find({}).exec();
+    }
+
+    getTagById(tagId: number): Tag 
+    {
+        return this.tagModel.findOne({id: tagId}).exec();
+    }
+
+    editTagById(tagId: number, tagContent: Tag): void 
+    {
+        this.tagModel.findOneAndUpdate({id: tagId}, tagContent).exec();
+    }
+
+    deleteTagById(tagId: number): void 
+    {
+        this.tagModel.findOneAndDelete({id: tagId}).exec();
+    }
+
+
+    addUser(user: User): void 
+    {
+        this.userModel.create(user);
+    }
+
+    editUserByLogin(login: string, user: User): void 
+    {
+        this.userModel.findOneAndUpdate({login: login}, user).exec();
+    }
+
+    getUserByLogin(login: string): User 
+    {
+        return this.userModel.findOne({login: login}).exec();
+    }
+
+    getUsers(): User[] 
+    {
+        return this.userModel.find({}).exec();
+    }
+
+    deleteUserByLogin(login: string): void 
+    {
+        this.userModel.findOneAndDelete({login: login}).exec();
     }
 
 }
