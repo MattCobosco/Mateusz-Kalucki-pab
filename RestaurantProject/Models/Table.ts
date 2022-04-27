e2e6fd573adced0752330426705c75598e51f085
MattCobosco <mateuszkalucki@gmail.com>
2022-04-27 17:23:56 +0200

    Table model

---
+++ RestaurantProject/Models/Table.ts
@@ -0,0 +1,20 @@
+class Table
+{
+    tableNumber: number;
+    seats: number;
+    status: TableStatus;
+
+    constructor(tableNumber: number, seats: number, status: TableStatus)
+    {
+        this.tableNumber = tableNumber;
+        this.seats = seats;
+        this.status = status;
+    }
+}
+
+enum TableStatus{
+    Available,
+    Occupied,
+    Dirty,
+    OutOfOrder
+}
