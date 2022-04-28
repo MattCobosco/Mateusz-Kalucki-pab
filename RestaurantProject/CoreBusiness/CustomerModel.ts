e166842780ed5903e89e504008628fc3213d2077
MattCobosco <mateuszkalucki@gmail.com>
2022-04-28 11:14:04 +0200

    added loyalty points

--- RestaurantProject/CoreBusiness/CustomerModel.ts
+++ RestaurantProject/CoreBusiness/CustomerModel.ts
@@ -4,6 +4,7 @@ class Customer
     email: string;
     phone: string;
     address: string;
+    loyaltyPoints: number;
 
     constructor(name: string, email: string, phone: string, address: string)
     {
@@ -11,8 +12,8 @@ class Customer
         this.email = email;
         this.phone = phone;
         this.address = address;
+        this.loyaltyPoints = 0;
     }
-
 }
 
 export default Customer;
