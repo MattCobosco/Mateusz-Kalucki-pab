4e768105ad5f6ce50fcbf50c170b5636454dc5f4
MattCobosco <mateuszkalucki@gmail.com>
2022-04-28 11:20:47 +0200

    Employee is bound to a Restaurant

--- RestaurantProject/CoreBusiness/EmployeeModel.ts
+++ RestaurantProject/CoreBusiness/EmployeeModel.ts
@@ -1,14 +1,18 @@
+import Restaurant from "./RestaurantModel";
+
 class Employee
 {
     name: string;
     surname: string;
     position: string
+    restaurant: Restaurant;
 
-    constructor(name: string, surname: string, position: string)
+    constructor(name: string, surname: string, position: string, restaurant: Restaurant)
     {
         this.name = name;
         this.surname = surname;
         this.position = position;
+        this.restaurant = restaurant;
     }
 }
 
