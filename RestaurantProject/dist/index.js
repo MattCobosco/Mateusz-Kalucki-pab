36c84ce8011c7c52f440b316a977b18a81fdaf78
MattCobosco <mateuszkalucki@gmail.com>
2022-04-30 17:08:04 +0200

    auto js

--- RestaurantProject/dist/index.js
+++ RestaurantProject/dist/index.js
@@ -10,29 +10,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require('mongoose');
-const Restaurant = require('./CoreBusiness/RestaurantModel');
+const { Restaurant } = require('./CoreBusiness/RestaurantModel');
 const express = require("express");
+const bodyParser = require("body-parser");
 const RestaurantRepository_1 = require("./DataStore/RestaurantRepository");
 const app = express();
 const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
 const restaurantRepository = new RestaurantRepository_1.RestaurantRepository();
 router.get('/restaurants', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let restaurants = yield restaurantRepository.getRestaurants();
-    if (restaurants) {
+    if (restaurants.length > 0)
         res.json(restaurants);
-    }
-    else {
+    else if (restaurants.length == 0)
+        res.status(200).send("Restaurant list is empty");
+    else
         res.status(404).send("No restaurants found");
-    }
 }));
 router.get('/restaurant/:name', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    let restaurant = yield restaurantRepository.getRestaurantByName(req.params.name);
-    if (restaurant) {
+    const restaurant = yield restaurantRepository.getRestaurantByName(req.params.name);
+    if (restaurant)
         res.json(restaurant);
-    }
-    else {
+    else
         res.status(404).send("Restaurant not found");
-    }
 }));
-app.use('/', router);
+router.delete('/restaurant/:name', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield restaurantRepository.deleteRestaurantByName(req.params.name);
+    res.status(200).send("Restaurant deleted");
+}));
+router.post('/restaurant', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const restaurant = req.body;
+    yield restaurantRepository.addRestaurant(restaurant);
+    res.status(200).send("Restaurant added");
+}));
 app.listen(3000);
