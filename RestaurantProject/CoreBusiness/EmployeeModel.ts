976ac97629a69c1ac26ae72f4281442c61435c64
MattCobosco <mateuszkalucki@gmail.com>
2022-05-02 09:58:38 +0200

    employeeId, restaurant to restaurantName

--- RestaurantProject/CoreBusiness/EmployeeModel.ts
+++ RestaurantProject/CoreBusiness/EmployeeModel.ts
@@ -1,18 +1,18 @@
-import Restaurant from "./RestaurantModel";
-
 class Employee
 {
+    employeeId: number;
     name: string;
     surname: string;
     position: string
-    restaurant: Restaurant;
+    restaurantName: string;
 
-    constructor(name: string, surname: string, position: string, restaurant: Restaurant)
+    constructor(employeeId: number, name: string, surname: string, position: string, restaurantName: string)
     {
+        this.employeeId = employeeId;
         this.name = name;
         this.surname = surname;
         this.position = position;
-        this.restaurant = restaurant;
+        this.restaurantName = restaurantName;
     }
 }
 
