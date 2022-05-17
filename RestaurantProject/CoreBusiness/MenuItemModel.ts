de0807bff01965cef850b6ae927b656c005ebe5d
MattCobosco <mateuszkalucki@gmail.com>
2022-05-17 20:49:53 +0200

    deleted ID

--- RestaurantProject/CoreBusiness/MenuItemModel.ts
+++ RestaurantProject/CoreBusiness/MenuItemModel.ts
@@ -11,16 +11,14 @@ enum Type
 
 class MenuItem
 {
-    menuItemId: number;
     name: string;
     price: number;
     type: Type;
     description: string;
     products: Product[];
 
-    constructor(menuItemId: number, name: string, price: number, type: Type, description: string, products: Product[])
+    constructor(name: string, price: number, type: Type, description: string, products: Product[])
     {
-        this.menuItemId = menuItemId;
         this.name = name;
         this.price = price;
         this.type = type;
